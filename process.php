fa7ff41088bbb16f9f4e94709d6f65e009513f27
Raisul Islam <mdraju3248@gmail.com>
2024-08-01 06:47:52 +0600

    inset data into the database

--- process.php
+++ process.php
@@ -3,12 +3,14 @@ include 'connect.php';
 
 if(isset($_POST['submit'])){
 
+    //variable for data
     $title = mysqli_real_escape_string($conn, $_POST['title']);
     $author = mysqli_real_escape_string($conn, $_POST['author']);
     $type = mysqli_real_escape_string($conn, $_POST['type']);
     $description = mysqli_real_escape_string($conn, $_POST['description']);
 
-    
+    //sql query
+    $sql = "INSERT INTO users(title, author, type, description) VALUES('$title', '$author', '$type', '$description')";
 
 }
 
