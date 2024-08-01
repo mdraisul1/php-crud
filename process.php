9f8f4a1d0834afe17855a5b25964832a9ca514f6
Raisul Islam <mdraju3248@gmail.com>
2024-08-01 06:41:57 +0600

    Add form submission handeling with SQL injection protection

--- process.php
+++ process.php
@@ -1,5 +1,14 @@
 <?php 
 include 'connect.php';
 
+if(isset($_POST['submit'])){
 
+    $title = mysqli_real_escape_string($conn, $_POST['title']);
+    $author = mysqli_real_escape_string($conn, $_POST['author']);
+    $type = mysqli_real_escape_string($conn, $_POST['type']);
+    $description = mysqli_real_escape_string($conn, $_POST['description']);
+
+    
+
+}
 
